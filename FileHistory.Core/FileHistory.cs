d329e2d82dd43092cbf983cc915ab25b89dfbf78
John <88855545+RedFiveCode@users.noreply.github.com>
2021-02-23 20:57:27 +0000

    Add unit tests for FileHistory class.

--- FileHistory.Core/FileHistory.cs
+++ FileHistory.Core/FileHistory.cs
@@ -1,26 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace FileHistory.Core
 {
-    public class FileHistory
-    {
+	public class FileHistory
+	{
+		private IFileSystem fileSystem;
+
+		public FileHistory() : this(new FileSystem())
+		{ }
+
+		public FileHistory(IFileSystem fileSystem)
+		{
+			this.fileSystem = fileSystem;
+		}
+		public List<FileHistoryGroup> GetFolderGroupDetails(string path)
+		{ 
+			return GetFolderGroupDetails(path, false, String.Empty, 0L);
+		}
+
 		public List<FileHistoryGroup> GetFolderGroupDetails(string path, bool recurseSubFolders, string wildcardFilter, long minimumFileSize)
 		{
+			if (String.IsNullOrEmpty(path))
+			{
+				throw new ArgumentNullException(nameof(path));
+			}
+
 			var wildcard = (String.IsNullOrEmpty(wildcardFilter) ? "*.*" : wildcardFilter);
 			var options = (recurseSubFolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
 
-			var files = Directory.EnumerateFiles(path, wildcard, options);
+			var files = fileSystem.Directory.EnumerateFiles(path, wildcard, options);
 
 			// get files in current folders
 			// and if recusrive, get sub-folders, and process each folder individually
 			// want to avoid any interference between groups of files in different folders that have the same name
 			if (recurseSubFolders)
 			{
-				Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+				fileSystem.Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
 
 				// for each folder
 				// get groups of files in that folder
@@ -67,27 +87,35 @@ namespace FileHistory.Core
 			return details;
 		}
 
-		public FileHistoryFile Split(string line)
+		public FileHistoryFile Split(string path)
 		{
+			if (String.IsNullOrEmpty(path))
+            {
+				throw new ArgumentNullException(nameof(path));
+            }
+
 			// does not match files with no extension
 			//var regex = new Regex(@".*\\(.*?) \((\d\d\d\d_\d\d_\d\d \d\d_\d\d_\d\d UTC)\)(\..*)?");
 			var regex = new Regex(@".*\\(?<name>.*?) \((?<ts>\d\d\d\d_\d\d_\d\d \d\d_\d\d_\d\d UTC)\)(?<ext>\..*)?");
 
-			if (regex.IsMatch(line))
+			if (regex.IsMatch(path))
 			{
-				var match = regex.Match(line);
+				var match = regex.Match(path);
 
 				FileHistoryFile details = null;
 
 				if (match.Groups.Count == 3)
                 {
-					details = new FileHistoryFile(line, match.Groups["name"].Value,
+					details = new FileHistoryFile(fileSystem,
+												    path,
+													match.Groups["name"].Value,
 													String.Empty,
 													match.Groups["ts"].Value);
                 }
 				else if (match.Groups.Count == 4) // optional extension
 				{
-					details = new FileHistoryFile(line,
+					details = new FileHistoryFile(fileSystem,
+												  path,
 												  match.Groups["name"].Value,
 												  match.Groups["ext"].Value,
 												  match.Groups["ts"].Value);
