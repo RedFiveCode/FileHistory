126b700865524886de0f8806f3fd218f7e145d16
John <John@Nectarine>
2021-02-26 17:15:50 +0000

    Add xml commenta.

--- FileHistory.Core/FileHistoryDiscovery.cs
+++ FileHistory.Core/FileHistoryDiscovery.cs
@@ -18,11 +18,25 @@ namespace FileHistory.Core
 		{
 			this.fileSystem = fileSystem;
 		}
+
+		/// <summary>
+		/// Get file history group objects for the specified folder
+		/// </summary>
+		/// <param name="path">Folder path</param>
+		/// <returns></returns>
 		public List<FileHistoryGroup> GetFolderGroupDetails(string path)
 		{ 
 			return GetFolderGroupDetails(path, false, String.Empty, 0L);
 		}
 
+		/// <summary>
+		/// Get file history group objects for the specified folder with options.
+		/// </summary>
+		/// <param name="path">Folder path</param>
+		/// <param name="recurseSubFolders">True to recuse sub-folders</param>
+		/// <param name="wildcardFilter">Optional wildcars; defaults to *.*</param>
+		/// <param name="minimumFileSize">Optional minimum file size; defaults to zero meaning all file sizes</param>
+		/// <returns></returns>
 		public List<FileHistoryGroup> GetFolderGroupDetails(string path, bool recurseSubFolders, string wildcardFilter, long minimumFileSize)
 		{
 			if (String.IsNullOrEmpty(path))
@@ -57,7 +71,7 @@ namespace FileHistory.Core
 		}
 
 		/// <summary>
-		/// Get file groups in the specified folder
+		/// Get file history groups in the specified folder
 		/// </summary>
 		/// <param name="path"></param>
 		/// <param name="wildcardFilter"></param>
@@ -94,28 +108,32 @@ namespace FileHistory.Core
 			return grouping;
 		}
 
-
-		public FileHistoryFile GetFileDetails(string path)
+		/// <summary>
+		/// Get the file history for the specified filename
+		/// </summary>
+		/// <param name="filename"></param>
+		/// <returns></returns>
+		public FileHistoryFile GetFileDetails(string filename)
 		{
-			if (String.IsNullOrEmpty(path))
+			if (String.IsNullOrEmpty(filename))
             {
-				throw new ArgumentNullException(nameof(path));
+				throw new ArgumentNullException(nameof(filename));
             }
 
 			// does not match files with no extension
 			//var regex = new Regex(@".*\\(.*?) \((\d\d\d\d_\d\d_\d\d \d\d_\d\d_\d\d UTC)\)(\..*)?");
 			var regex = new Regex(@".*\\(?<name>.*?) \((?<ts>\d\d\d\d_\d\d_\d\d \d\d_\d\d_\d\d UTC)\)(?<ext>\..*)?");
 
-			if (regex.IsMatch(path))
+			if (regex.IsMatch(filename))
 			{
-				var match = regex.Match(path);
+				var match = regex.Match(filename);
 
 				FileHistoryFile details = null;
 
 				if (match.Groups.Count == 3)
                 {
 					details = new FileHistoryFile(fileSystem,
-												    path,
+												    filename,
 													match.Groups["name"].Value,
 													String.Empty,
 													match.Groups["ts"].Value);
@@ -123,7 +141,7 @@ namespace FileHistory.Core
 				else if (match.Groups.Count == 4) // optional extension
 				{
 					details = new FileHistoryFile(fileSystem,
-												  path,
+												  filename,
 												  match.Groups["name"].Value,
 												  match.Groups["ext"].Value,
 												  match.Groups["ts"].Value);
