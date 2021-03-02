acd7a60cfcb886fd752022309cece69413167af5
John <88855545+RedFiveCode@users.noreply.github.com>
2021-03-02 19:09:52 +0000

    Refactor - Add unit testable TidyFilter class.

---
+++ FileHistory.Core/FilesToKeepAndDelete.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace FileHistory.Core
+{
+    public class FilesToKeepAndDelete
+	{
+		public FilesToKeepAndDelete(List<FileHistoryFile> keepList, List<FileHistoryFile> deleteList)
+        {
+			KeepList = keepList;
+			DeleteList = deleteList;
+        }
+
+		public IList<FileHistoryFile> KeepList { get; private set; }
+
+		public IList<FileHistoryFile> DeleteList { get; private set; }
+	}
+}
---
+++ FileHistory.Core/TidyFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FileHistory.Core
+{
+
+    public class TidyFilter
+    {
+		public FilesToKeepAndDelete GetFilesToKeepAndDelete(FileHistoryGroup group, int recentGenerationsToKeep)
+		{
+			if (group == null)
+			{
+				throw new ArgumentNullException(nameof(group));
+			}
+
+			if (recentGenerationsToKeep < 0)
+			{
+				throw new ArgumentOutOfRangeException(nameof(recentGenerationsToKeep));
+			}
+
+			var keepList = new List<FileHistoryFile>();
+			var deleteList = new List<FileHistoryFile>();
+
+			// ignore if only one file, cannot have duplicates
+			if (group.Files.Count == 1)
+			{
+				keepList.Add(group.Files.First());
+			}
+			else
+			{
+				// files are ordered by time, keep the newest (first/Skip(1)) and delete the others
+				var files = group.Files.OrderByDescending(x => x.CreationTime);
+
+				keepList.AddRange(files.Take(recentGenerationsToKeep));
+
+				deleteList.AddRange(files.Skip(recentGenerationsToKeep));
+			}
+
+			return new FilesToKeepAndDelete(keepList, deleteList);
+		}
+	}
+}
