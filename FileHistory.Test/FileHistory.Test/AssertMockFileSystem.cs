0c07b5890e1a939339b019ca204671e77d272bef
John <88855545+RedFiveCode@users.noreply.github.com>
2021-04-25 16:14:17 +0100

    Add unit tests to TidyOperation

---
+++ FileHistory.Test/FileHistory.Test/AssertMockFileSystem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO.Abstractions.TestingHelpers;
+
+public class AssertMockFileSystem
+{ 
+    public static void AssertFileExists(MockFileSystem mockFileSystem, string filename)
+    {
+        var exists = mockFileSystem.FileExists(filename);
+
+        Assert.IsTrue(exists, $"File ${filename} has been deleted when should exist");
+    }
+
+    public static void AssertFileNotExist(MockFileSystem mockFileSystem, string filename)
+    {
+        var exists = mockFileSystem.FileExists(filename);
+
+        Assert.IsFalse(exists, $"File ${filename} exists when should be deleted");
+    }
+}
