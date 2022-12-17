34a0f5c9c3ef601958ac9e2b7960daf7abbf8678
John <88855545+RedFiveCode@users.noreply.github.com>
2022-12-17 17:34:18 +0000

    Update AssemblyInfo year

--- FileHistory.Test/FileHistory.Test/Properties/AssemblyInfo.cs
+++ FileHistory.Test/FileHistory.Test/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RedFiveCode (https://github.com/RedFiveCode)")]
 [assembly: AssemblyProduct("FileHistory.Test")]
-[assembly: AssemblyCopyright("Copyright © 2021 RedFiveCode (https://github.com/RedFiveCode)")]
+[assembly: AssemblyCopyright("Copyright © 2021-2022 RedFiveCode (https://github.com/RedFiveCode)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
