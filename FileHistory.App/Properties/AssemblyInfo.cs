e6a9d1de1894a70465c6c87f9e31bf0b224c0c3d
John <88855545+RedFiveCode@users.noreply.github.com>
2025-01-20 21:25:14 +0000

    Upgrade to .net 9.0

--- FileHistory.App/Properties/AssemblyInfo.cs
+++ FileHistory.App/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("FileHistory")]
-[assembly: AssemblyDescription("FileHistory viewer")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RedFiveCode (https://github.com/RedFiveCode)")]
-[assembly: AssemblyProduct("FileHistory")]
-[assembly: AssemblyCopyright("Copyright © 2021-2025 RedFiveCode (https://github.com/RedFiveCode)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1a3e4a34-8ded-4dd5-a875-8a3a9450a05b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- FileHistory.Test/FileHistory.Test/Properties/AssemblyInfo.cs
+++ FileHistory.Test/FileHistory.Test/Properties/AssemblyInfo.cs
@@ -1,19 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("FileHistory.Test")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RedFiveCode (https://github.com/RedFiveCode)")]
-[assembly: AssemblyProduct("FileHistory.Test")]
-[assembly: AssemblyCopyright("Copyright © 2021-2025 RedFiveCode (https://github.com/RedFiveCode)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("dbda4799-3a8f-42d5-b4e3-03256573fc9d")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
