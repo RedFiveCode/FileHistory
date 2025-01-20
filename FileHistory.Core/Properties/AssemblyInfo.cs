beec0f17aa0eef208e017bd07c4aef586ef56a4e
John <88855545+RedFiveCode@users.noreply.github.com>
2025-01-20 18:54:33 +0000

    Update copyright year.

--- FileHistory.Core/Properties/AssemblyInfo.cs
+++ FileHistory.Core/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RedFiveCode (https://github.com/RedFiveCode)")]
 [assembly: AssemblyProduct("FileHistoryCore")]
-[assembly: AssemblyCopyright("Copyright © 2021-2024 RedFiveCode (https://github.com/RedFiveCode)")]
+[assembly: AssemblyCopyright("Copyright © 2021-2025 RedFiveCode (https://github.com/RedFiveCode)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
