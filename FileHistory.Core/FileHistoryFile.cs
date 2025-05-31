18d00113498bc9e02ff05a4a5389d23884dec700
John <88855545+RedFiveCode@users.noreply.github.com>
2025-05-31 17:17:58 +0100

    Refactor - Use init, remove ctor duplication

--- FileHistory.Core/FileHistoryFile.cs
+++ FileHistory.Core/FileHistoryFile.cs
@@ -7,16 +7,9 @@ namespace FileHistory.Core
     [DebuggerDisplay("Name={Name}, Extension={Extension}, Time={Time}, FullPath={FullPath}")]
     public class FileHistoryFile
     {
-
         public FileHistoryFile(IFileSystem fileSystem, string path, string name, string extension, string timestamp)
+            : this(fileSystem, path, name, extension, timestamp, fileSystem.FileInfo.New(path).CreationTime)
         {
-            FullPath = path;
-            Name = name;
-            Extension = extension;
-            Time = timestamp;
-            var info = fileSystem.FileInfo.New(path);
-
-            CreationTime = info.CreationTime;
         }
 
         public FileHistoryFile(IFileSystem fileSystem, string path, string name, string extension, string timestamp, DateTime created)
@@ -27,8 +20,7 @@ namespace FileHistory.Core
             Time = timestamp;
             CreationTime = created;
 
-            // the file creation timestamp in the filename may differ from the timestamp from the file info object
-            // so only use the file info object to get the file size
+            // Always set Length using file info
             var info = fileSystem.FileInfo.New(path);
             Length = info.Length;
         }
@@ -36,22 +28,22 @@ namespace FileHistory.Core
         /// <summary>
         /// Pathname 
         /// </summary>
-        public string FullPath { get; private set; }
+        public string FullPath { get; init; }
 
         /// <summary>
         /// Filename 
         /// </summary>
-        public string Name { get; private set; }
+        public string Name { get; init; }
 
         /// <summary>
         /// File extension; optional (includes dot) 
         /// </summary>
-        public string Extension { get; private set; }
+        public string Extension { get; init; }
 
         /// <summary>
         /// File timestamp
         /// </summary>
-        public string Time { get; private set; }
+        public string Time { get; init; }
 
         /// <summary>
         /// Original Filename; includes timestamp.
@@ -72,11 +64,11 @@ namespace FileHistory.Core
         /// <summary>
         /// Gets the size, in bytes, of the current file
         /// </summary>
-        public long Length { get; private set; }
+        public long Length { get; init; }
 
         /// <summary>
         /// Get the creation time of the file
         /// </summary>
-        public DateTime CreationTime { get; private set; }
+        public DateTime CreationTime { get; init; }
     }
 }
